f9342d87db48fdfe15c90e1707d035779f01ae2b
yashjadhav7984622997 <yashjadhav8460318684@gmail.com>
2024-02-16 15:02:49 +0530

    add

--- faculty/urls.py
+++ faculty/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/FacultyHomePage/",views.FacultyHomePage,name="FacultyHomePage"),
+    path("FacultyHomePage/",views.FacultyHomePage,name="FacultyHomePage"),
     path("",views.login,name="login"),
     path("",views.logout,name="logout"),
 ]
