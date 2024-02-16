95e49587446762e2ce0ccfa878c09c8605c3f795
yashjadhav7984622997 <yashjadhav8460318684@gmail.com>
2024-02-16 11:46:31 +0530

    add

--- faculty/urls.py
+++ faculty/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.FacultyHomePage,name="FacultyHomePage")
-
+    path("/FacultyHomePage/",views.FacultyHomePage,name="FacultyHomePage"),
+    path("",views.login,name="login"),
+    path("",views.logout,name="logout"),
 ]
--- faculty/views.py
+++ faculty/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def FacultyHomePage(request):
     return render(request,FacultyHomePage.html)
+
+def login(request):
+    return render(request,login.html)
+
+def logout(request):
+    return render(request,logout.html)         
