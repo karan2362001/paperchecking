66a995dcd1c45c4ca6551898d1471262fba31868
karan <patanikaran@gmail.com>
2024-02-16 11:43:48 +0530

    adddd

--- org/urls.py
+++ org/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.adminHomepage,name="adminHomepage")
+    path("/adminHomepage/",views.adminHomepage,name="adminHomepage"),
+    path("",views.login,name="login"),
+    path("/logout/",views.logout, name='logout'),
  
     
 ]
--- org/views.py
+++ org/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib.auth import authenticate,login
+from django.contrib import messages 
+from api.models import CustomUser
 
 def adminHomepage(request):
     return render(request,"adminHomepage.html")
+
+def login(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('adminHomepage')
+        else:
+            messages.info(request,"invalid credentials...")
+            return redirect('login')
+    else:
+        return render(request,"login.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request,"login.html")
